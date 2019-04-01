8dfa31e37c1c34f8f9ceebe7b94dc6b4fbb89370
Franco Melandri <franco.melandri@gmail.com>
2019-04-01 21:18:40 +0200

    adding preliminar UI...not so good...hate CSS :(

--- src/yase-ui/src/App.js
+++ src/yase-ui/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +6,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Yet Another Shortener Engine
         </header>
+        <body className="App-body">
+          <span className="App-label">Insert URL</span>
+          <span><input type="text" className="App-text"></input></span>
+          <span><button id="create" className="App-button">CREATE</button></span>
+        </body>
+        <footer className="App-footer">
+        </footer>
       </div>
     );
   }
