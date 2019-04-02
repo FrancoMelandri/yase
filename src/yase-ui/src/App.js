8559e20432e4596f384a53c69e6fdce601367106
Franco Melandri <franco.melandri@gmail.com>
2019-04-02 04:24:55 +0200

    introducing axios for REST call

--- src/yase-ui/src/App.js
+++ src/yase-ui/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+const axios = require('axios')
 
 class App extends Component {
+
+  onCreate() {
+    alert('Here I am')
+    axios.get('http://www.google.com')
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           Yet Another Shortener Engine
         </header>
-        <body className="App-body">
-          <span className="App-label">Insert URL</span>
-          <span><input type="text" className="App-text"></input></span>
-          <span><button id="create" className="App-button">CREATE</button></span>
-        </body>
+        <div className="App-body">
+            <span className="App-label">Insert URL</span>
+            <span><input type="text" className="App-text"></input></span>
+            <span><button id="create" className="App-button" onClick={this.onCreate}>Create</button></span>
+        </div>
         <footer className="App-footer">
         </footer>
       </div>
